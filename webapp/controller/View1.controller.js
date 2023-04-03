34cfdee4e00c374f0eef4766a7a4f3bdd93f4bbe
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-04-03 16:27:03 +0530

    Download excell sheet added

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,13 +1,16 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	'sap/ui/export/library',
+	'sap/ui/export/Spreadsheet'
+], function(Controller,exportLibrary,Spreadsheet) {
 	"use strict";
+		var EdmType = exportLibrary.EdmType;
 
 	return Controller.extend("GithubSAP-UI5-Fiori-7_Element-Binding.controller.View1", {
 		
 		onInit : function(){
-			var oModel = new sap.ui.model.json.JSONModel();
-			oModel.loadData("model/empdata.json");
+			var oModel = new sap.ui.model.json.JSONModel("model/empdata.json");
+			// oModel.loadData("model/empdata.json");
 			this.getView().setModel(oModel);
 		},
 		
@@ -48,6 +51,75 @@ sap.ui.define([
 			oform.bindElement(data);
 			
 			
+		},
+		
+		createColumnConfig: function() {
+			var aCols = [];
+
+			aCols.push({
+				label: 'First name',
+				property: 'firstName',
+				type: EdmType.String
+			
+			});
+
+			aCols.push({
+				label: 'Last name',
+				type: EdmType.String,
+				property: 'lastName'
+				
+			});
+
+			aCols.push({
+				label: 'Age',
+				property: 'age',
+				type: EdmType.Number
+			});
+
+			aCols.push({
+				label: 'Pos',
+				property: 'position',
+				type: EdmType.String
+			});
+
+			aCols.push({
+				label: 'Salary',
+				property: 'salary',
+				type: EdmType.Number
+			});
+
+		
+
+
+
+			return aCols;
+		},
+
+		onDownload: function() {
+			var aCols, oRowBinding, oSettings, oSheet, oTable;
+
+			if (!this._oTable) {
+				this._oTable = this.byId('employees');
+			}
+
+			oTable = this._oTable;
+			oRowBinding = oTable.getBinding();
+			aCols = this.createColumnConfig();
+
+			oSettings = {
+				workbook: {
+					columns: aCols,
+					hierarchyLevel: 'Level'
+				},
+				dataSource: oRowBinding,
+				fileName: 'Table export sample.xlsx',
+				worker: false // We need to disable worker because we are using a MockServer as OData Service
+			};
+
+			oSheet = new Spreadsheet(oSettings);
+			oSheet.build().finally(function() {
+				oSheet.destroy();
+			});
 		}
 
 	});
